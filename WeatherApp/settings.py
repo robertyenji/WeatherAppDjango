ba72cf5ed056ebedc253371d8ff37350d101053c
Robert Yenji <92427429+robertyenji@users.noreply.github.com>
2024-07-02 15:00:52 -0400

    Update settings.py 2

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -75,14 +75,17 @@ WSGI_APPLICATION = 'WeatherApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DBNAME'),
+        'HOST': os.environ.get('DBHOST'),
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASS'),
+        "PORT": "5432",
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
